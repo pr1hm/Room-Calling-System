e6d8e7858106e0f91c92b021aead3c11061c340b
S. Pratham <educate.pratham@gmail.com>
2026-06-11 12:43:52 +0530

    Implement DashboardServer to handle HTTP requests and update ServerActivity for server management

--- app/src/main/java/com/pratham/roomcalling/ServerActivity.java
+++ app/src/main/java/com/pratham/roomcalling/ServerActivity.java
@@ -1,24 +1,36 @@
 package com.pratham.roomcalling;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import com.pratham.roomcalling.http.DashboardServer;
+import java.io.IOException;
 
 public class ServerActivity extends AppCompatActivity {
+
+    private DashboardServer dashboardServer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_server);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        dashboardServer = new DashboardServer();
+        try {
+            dashboardServer.start();
+            Log.d("HTTP_TEST", "Server started on port 8080");
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.e("HTTP_TEST", "Could not start server", e);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (dashboardServer != null) {
+            dashboardServer.stop();
+            Log.d("HTTP_TEST", "Server stopped");
+        }
     }
 }
--- app/src/main/java/com/pratham/roomcalling/http/DashboardServer.java
+++ app/src/main/java/com/pratham/roomcalling/http/DashboardServer.java
@@ -1,4 +1,25 @@
 package com.pratham.roomcalling.http;
 
-public class DashboardServer {
+import fi.iki.elonen.NanoHTTPD;
+
+public class DashboardServer extends NanoHTTPD {
+
+    // The SRS specifies the server must run on port 8080
+    public DashboardServer() {
+        super(8080);
+    }
+
+    @Override
+    public Response serve(IHTTPSession session) {
+        String uri = session.getUri();
+
+        // Route: http://<server-ip>:8080/help
+        if (uri.equals("/help")) {
+            String responseStr = "<html><body><h1>Hello World from Server!</h1><p>The NanoHTTPD server is running.</p></body></html>";
+            return newFixedLengthResponse(Response.Status.OK, "text/html", responseStr);
+        }
+
+        // Default fallback for any other URL
+        return newFixedLengthResponse(Response.Status.NOT_FOUND, "text/plain", "404 Not Found");
+    }
 }
