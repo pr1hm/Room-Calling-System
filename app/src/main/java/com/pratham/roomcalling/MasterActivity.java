5e5e53a6d235633bb96d349c08fca3ff50bde5a6
S. Pratham <educate.pratham@gmail.com>
2026-06-11 12:41:53 +0530

    Add ClientActivity, MasterActivity, and ServerActivity with layout files for each

---
+++ app/src/main/java/com/pratham/roomcalling/MasterActivity.java
@@ -0,0 +1,24 @@
+package com.pratham.roomcalling;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MasterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_master);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
